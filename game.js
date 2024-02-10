ba08d8bbb5b11fd4ef27aa760d2e80c63cf25304
KevinMeekel <144624233+KevinMeekel@users.noreply.github.com>
2024-02-10 21:49:10 +0100

    Change player input method to buttons

--- game.js
+++ game.js
@@ -10,30 +10,43 @@ function playRound(playerSelection, computerSelection) {
     console.log("Player chose " + playerSelection);
     console.log("Computer chose " + computerSelection);
     if (playerSelection === computerSelection) {
-        return "It's a tie!";
+        console.log("It's a tie!");
+        return;
     } else if (playerSelection === "ROCK") {
         if (computerSelection === "SCISSORS") {
-            return "ROCK beats SCISSORS; Player wins!"
+            console.log("ROCK beats SCISSORS; Player wins!");
+            return;
         } else {
-            return "PAPER beats ROCK; Computer wins!"
+            console.log("PAPER beats ROCK; Computer wins!");
+            return; 
         }
     } else if (playerSelection === "SCISSORS") {
         if (computerSelection === "ROCK") {
-            return "ROCK beats SCISSORS; Computer wins!"
+            console.log("ROCK beats SCISSORS; Computer wins!");
+            return;
         } else {
-            return "SCISSORS beat PAPER; Player wins!"
+            console.log("SCISSORS beat PAPER; Player wins!");
+            return;
         }
     } else { //if PLAYER picks PAPER//
         if (computerSelection === "ROCK") {
-            return "PAPER beats ROCK; Player wins!"
+            console.log("PAPER beats ROCK; Player wins!");
+            return;
         } else {
-            return "SCISSORS beat PAPER; Computer wins!"
+            console.log("SCISSORS beat PAPER; Computer wins!");
+            return;
         }
     }
 }
 
-let userInput = prompt('Enter "Rock", "Paper" or "Scissors" (not case sensitive)', undefined);
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorsButton = document.getElementById("scissors");
 
-let playerChoice = userInput.toUpperCase();
-
-console.log(playRound(playerChoice, getComputerChoice()));
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        const playerChoice = e.target.textContent.toUpperCase();
+        playRound(playerChoice, getComputerChoice());
+    });
+});
