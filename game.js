303daff60167944481e2d9faed2492ffc4dd91e7
KevinMeekel <144624233+KevinMeekel@users.noreply.github.com>
2024-02-11 22:33:34 +0100

    Add div displaying single game results

--- game.js
+++ game.js
@@ -1,5 +1,13 @@
 const options = ["ROCK", "PAPER", "SCISSORS"];
 
+const resultsDiv = document.querySelector('.results');
+const choice1 = document.createElement('h2');
+const choice2 = document.createElement('h2');
+const winner = document.createElement('h1');
+resultsDiv.appendChild(choice1);
+resultsDiv.appendChild(choice2);
+resultsDiv.appendChild(winner);
+
 function getComputerChoice() {
     const roll = Math.floor(Math.random() * options.length);
     const pick = options[roll];    
@@ -7,41 +15,41 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    console.log("Player chose " + playerSelection);
-    console.log("Computer chose " + computerSelection);
+    choice1.textContent = ("Player chose " + playerSelection);
+    choice2.textContent = ("Computer chose " + computerSelection);
     if (playerSelection === computerSelection) {
-        console.log("It's a tie!");
+        winner.textContent = "It's a tie!";
         return;
     } else if (playerSelection === "ROCK") {
         if (computerSelection === "SCISSORS") {
-            console.log("ROCK beats SCISSORS; Player wins!");
+            winner.textContent = "ROCK beats SCISSORS; Player wins!";
             return;
         } else {
-            console.log("PAPER beats ROCK; Computer wins!");
+            winner.textContent = "PAPER beats ROCK; Computer wins!";
             return; 
         }
     } else if (playerSelection === "SCISSORS") {
         if (computerSelection === "ROCK") {
-            console.log("ROCK beats SCISSORS; Computer wins!");
+            winner.textContent = "ROCK beats SCISSORS; Computer wins!";
             return;
         } else {
-            console.log("SCISSORS beat PAPER; Player wins!");
+            winner.textContent = "SCISSORS beat PAPER; Player wins!";
             return;
         }
     } else { //if PLAYER picks PAPER//
         if (computerSelection === "ROCK") {
-            console.log("PAPER beats ROCK; Player wins!");
+            winner.textContent = "PAPER beats ROCK; Player wins!";
             return;
         } else {
-            console.log("SCISSORS beat PAPER; Computer wins!");
+            winner.textContent = "SCISSORS beat PAPER; Computer wins!";
             return;
         }
     }
 }
 
-const rockButton = document.getElementById("rock");
-const paperButton = document.getElementById("paper");
-const scissorsButton = document.getElementById("scissors");
+// const rockButton = document.getElementById("rock");
+// const paperButton = document.getElementById("paper");
+// const scissorsButton = document.getElementById("scissors");
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach(button => {
